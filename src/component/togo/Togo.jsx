5083e7cdb534a276003095e3937c9bb6326f4d5e
mrcau <mrcau98@gmail.com>
2021-02-11 08:09:09 +0900

    '0221'

--- src/component/togo/Togo.jsx
+++ src/component/togo/Togo.jsx
@@ -6,6 +6,8 @@ import problem from './problem.png';
 import anlysis from './analysis.png';
 import idea from './idea.png';
 import solution from './solution.png';
+import tool from './tool.png';
+import rocket from './rocket.png';
 
 import Opentoolbox from '../opentool/Opentoolbox';
 
@@ -13,18 +15,20 @@ import Opentoolbox from '../opentool/Opentoolbox';
 function Togo({ fireApp, user, userInfo,  }) {
 const history = useHistory();
   return (
-    <div className="togo" style={{paddingBottom:"20px"}}>      
-      <div className="jumbo" style={{width:"50%"}}>
-        <Jumbotron>
-          <h1>디자인씽킹!, 기업가정신!</h1>
-          <p>컴퓨터와 인공지능 기술로 대체 될 수 없는 인간 고유의 <br/>
-          창의성과 공감능력을 통해 틀에 박힌 문제가 아닌 다양한 문제를 스스로 만들어 내고 
-          답을 찾을 수 능력을 길러주는 기업가정신 교육</p>
-          <p><Button variant="primary">Learn more</Button></p>
+    <div className="togo" style={{paddingBottom:"10px"}}>      
+      <div className="togoTop" style={{display:"flex"}} > 
+        <Jumbotron className="jumbo" style={{background:"none",flex:"1"}}>
+          <h1 style={{fontWeight:"900",color:"var(--Acolor)"}}>협업! 도전!</h1>
+          <p>디자인씽킹 기반의 협업도구로 기발한 아이디어와 도전을 이끌어내 보세요.
+             자신만의 도구를 만들고 더욱 편리하게 그룹별 공동작업을 시작할 수 있습니다.
+          </p>
+          <p>
+            <button className="btn1" style={{width:"150px"}}>브레인스톰</button>
+            </p>
         </Jumbotron> 
-      </div>    
+        <div className="togoback" />
+      </div>
       <div className="togoMain">          
-        <div className="cardWrap">
         <Card className="mainCard" style={{paddingTop:"10px"}} >
           <Card.Img variant="top" src={problem} style={{width:"8rem",height:"7rem",margin:"auto"}}/>
           <Card.Body>
@@ -32,21 +36,22 @@ const history = useHistory();
             <Card.Text>
             다양한 관점의 관찰과 공감을 통해 문제 상황을 인식하고 해결의 <br/> 실마리를 찾을 수 있습니다. 
             </Card.Text>
-            <Button variant="primary">바로가기</Button>
+            <button className="btn1">바로가기</button>
+
           </Card.Body>
         </Card>      
-        <Card className="mainCard" style={{marginLeft:"2vw",paddingTop:"10px"}}>
+        <Card className="mainCard" style={{paddingTop:"10px"}}>
           <Card.Img variant="top"  src={anlysis} style={{width:"8rem",height:"7rem",margin:"auto"}} />
           <Card.Body>
             <Card.Title style={{fontWeight:"900"}}>데이터분석</Card.Title>
             <Card.Text>
             데이터 수집/분석은 문제해결을<br/> 위한 최적의 판단을 내리는 과학적 근거가 됩니다.
             </Card.Text>
-            <Button  variant="primary">바로가기</Button>
+            <button className="btn1">바로가기</button>
+
           </Card.Body>
         </Card>
-        </div>
-        <div className="cardWrap" >
+        
         <Card  className="mainCard" style={{paddingTop:"10px"}}>
           <Card.Img variant="top" src={idea} style={{width:"8rem",height:"7rem",margin:"auto"}} />
           <Card.Body>
@@ -54,44 +59,45 @@ const history = useHistory();
             <Card.Text>
             유연한 사고를 이끌어 내는 창의적 발상법을 통해 다양한 아이디어를 생각할 수 있습니다.
             </Card.Text>
-            <Button variant="primary" onClick={() =>history.push('/scamper')} >바로가기</Button>
+            <button className="btn1" onClick={() =>history.push('/scamper')} >바로가기</button>
           </Card.Body>
         </Card>
-        <Card  className="mainCard" style={{marginLeft:"2vw",paddingTop:"10px"}}>
+        <Card  className="mainCard" style={{paddingTop:"10px"}}>
           <Card.Img variant="top" src={solution} style={{width:"8rem",height:"7rem",margin:"auto"}} />
           <Card.Body>
             <Card.Title style={{fontWeight:"900"}}>문제해결</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
+            아이디어를 시각·촉각으로 느낄 수 시제품 (Prototype) 을 제작하여 생각을 구체화하고 확장해 나갑니다. 
             </Card.Text>
-            <Button variant="primary">바로가기</Button>
+            <button className="btn1">바로가기</button>
+
           </Card.Body>
         </Card>
-        </div>
+        <Card  className="mainCard" style={{paddingTop:"10px"}}>
+          <Card.Img variant="top" src={rocket} style={{width:"7rem",height:"7rem",margin:"auto"}} />
+          <Card.Body>
+            <Card.Title style={{fontWeight:"900"}}>스타트업</Card.Title>
+            <Card.Text>
+            문제해결을 위해 '새로움'을 만들고 '도전'하는 '기업가정신'으로 혁신적인 사업을 창출합니다.
+            </Card.Text>
+            <button className="btn1">바로가기</button>
+
+          </Card.Body>
+        </Card>
+        <Card  className="mainCard" style={{paddingTop:"10px"}}>
+          <Card.Img variant="top" src={tool} style={{width:"8rem",height:"7rem",margin:"auto"}} />
+          <Card.Body>
+            <Card.Title style={{fontWeight:"900"}}>도구의 인간</Card.Title>
+            <Card.Text>
+              급변하는 새시대에 나침반 역할을 하는 강력한 디지털 도구로 새로운 길을 개척해보세요.
+            </Card.Text>
+            <button className="btn1">바로가기</button>
+          </Card.Body>
+        </Card>
       </div>
-      {userInfo.level>0 &&
-      <div className="cardWrap" style={{width:"90vw",margin:"auto"}} >
-        <div className="mainOpenTool" style={{width:"48%",marginRight:"20px"}} >
-          <Opentoolbox fireApp={fireApp} user={user} userInfo={userInfo}/>
-        </div>
-        <div className="mainOpenTool" style={{width:"48%",margin:"auto"}} >
-          <Opentoolbox fireApp={fireApp} user={user} userInfo={userInfo}/>
-        </div>
-      </div>
-      }
-
-    </div>  );}export default Togo;
 
 
+    </div>  );}    
+    export default Togo;
 
-        // <button className="btnMenu" onClick={() => history.push('/scamper')}>스캠퍼</button>
-        // <button className="btnMenu">브레인스톰</button>
-        // <button className="btnMenu">트리즈</button>
-        // <button className="btnMenu">5Why</button>
-        // <button className="btnMenu">관찰</button>
-        // <button className="btnMenu">만다라트</button>
-        // <button className="btnMenu">SWOT</button>
-        // <button className="btnMenu">린캔버스</button>
-        // <button className="btnMenu">페르소나</button>
-        // <button className="btnMenu">프로토타입</button>
+
