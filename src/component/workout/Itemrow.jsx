f372264206fbe43d5b47b7b77d7225e521002e8f
mrcau <mrcau@naver.com>
2021-05-11 16:27:27 +0900

    0511

--- src/component/workout/Itemrow.jsx
+++ src/component/workout/Itemrow.jsx
@@ -3,6 +3,7 @@ import React, { memo } from 'react';
 import { Card } from 'react-bootstrap';
 import {  DeleteForever, } from '@material-ui/icons';
 import { IconButton } from '@material-ui/core';
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
 
 function  Itemrow ({item,fireTodo,todayId}) {
   const itemSet = Object.values(item);
@@ -28,10 +29,12 @@ function  Itemrow ({item,fireTodo,todayId}) {
   }
   return (
     <div className="samtoolitemrow">      
-      <Card bg={itemSet[0].color} text={'white'} style={{ width: '100%', height:'120px'}} className="mb-2" >      
+    {/* height:'120px' */}
+      <Card bg={itemSet[0].color} text={'white'} style={{ width: '100%', }} className="mb-2" >      
         <Card.Header style={{display:'flex',justifyContent:"space-between",padding:'2px',height:'30px'}} >
-          <IconButton style={{width:'20px', height:'15px'}} > <DeleteForever onClick={itemDel} style={{color:'white'}} /></IconButton>         
+          
           <div>
+          <IconButton style={{width:'20px', height:'15px'}} > <DeleteForever onClick={itemDel} style={{color:'white'}} /></IconButton>         
           {itemSet[0].today}
           </div> 
           <div>{itemSet[0].body}</div>
@@ -41,14 +44,25 @@ function  Itemrow ({item,fireTodo,todayId}) {
           <button className="btn btn2 btnRoomLink" onClick={minus} >-</button>
           </div>
         </Card.Header>      
-
+        {/* height:"80px", */}
         <div className="cardTitle" style={{textAlign:"center"}}>
-          <Card.Body style={{padding:"8px",height:"80px",overflowY:"auto"}}>
+          <Card.Body style={{padding:"8px",overflowY:"auto"}}>
             {
               Object.values(itemSet).map((e,i) => { 
-                return <Card.Text style={{fontSize:"14px",lineHeight:"5px",padding:"0",textAlign:'left'  }}> 
-                {i+1} - {e.gameSelect} {e.workoutSet||1}세트  {e.workWeight||1}kg {e.workRepeat||1}회 
-                {/* {e.workRepeat.map((e)=>{return e})}  */}
+                return <Card.Text style={{fontSize:"16px",lineHeight:"20px",padding:"0",textAlign:'left'  }}> 
+                💪 {e.gameSelect} {e.workoutSet||1}세트 총   
+                {
+                  e.workRepeat.reduce(function add(sum, currValue) { return sum + currValue; }, 0)
+                }회
+                {<br/>}
+                {/* {e.workWeight||1}kg  */}
+                {/* {e.workRepeat||1}회 <br/> */}
+                {e.workWeight[0] !==0 && <FitnessCenterIcon/> } 
+                {e.workWeight[0] !==0 && "무게 : " } 
+                {e.workWeight[0] !==0 && e.workWeight.map((e)=>{return e + 'kg '})} 
+                {e.workWeight[0] !==0 && <br/>} 
+                 {/* <FitnessCenterIcon/> 무게 : {e.workWeight.map((e)=>{return e + 'kg '})}  */}
+                 <FitnessCenterIcon/> 횟수 : {e.workRepeat.map((e)=>{return e + '회 '})} 
                 
                 </Card.Text>
               })
